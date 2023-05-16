cc6db4fc301177a25fb01fd524cbbaeaa62e5a11
suzan0405 <92138223+suzan0405@users.noreply.github.com>
2023-05-16 12:33:12 +0900

    Update UBERStudent20191011.java

--- HW1/UBERStudent20191011.java
+++ HW1/UBERStudent20191011.java
@@ -11,64 +11,80 @@ import org.apache.hadoop.mapreduce.*;
 import org.apache.hadoop.mapreduce.lib.input.*;
 import org.apache.hadoop.mapreduce.lib.output.*;
 import org.apache.hadoop.util.GenericOptionsParser;
+// import org.apache.hadoop.fs.FSDataInputStream;
+// import org.apache.hadoop.fs.FSDataOutputStream;
 
 public class UBERStudent20191011
 {
+	public static String dayOfWeek(int day){
+		switch (day) {
+				case 1:
+					return "MON";
+				case 2:
+					return "TUE";
+				case 3:
+					return "WEN";
+				case 4:
+					return "THR";
+				case 5:
+					return "FRI";
+				case 6:
+					return "SAT";
+				default:
+					return "SUN";
+			}
+	}
+	
 	public static class UBERStudent20191011Mapper extends Mapper<Object, Text, Text, Text>{
-		private Text one_key = new Text();
-		private Text one_value = new Text();
+		private Text word = new Text();
+		private Text value = new Text();
 		
 		public void map(Object key, Text value, Context context) throws IOException, InterruptedException {
-			String s = value.toString();
-			String[] token = s.split(",");
+			StringTokenizer itr = new StringTokenizer(value.toString(), ",");
+			String baseNum = itr.nextToken().trim();
 			
-			StringTokenizer itr = new StringTokenizer(token[1], "/");
-			int month = Integer.parseInt(itr.nextToken());
-			int day = Integer.parseInt(itr.nextToken());
-			int year = Integer.parseInt(itr.nextToken());
+			String date = itr.nextToken().trim();
+			String[] d = date.split("/");
+			int month = d[0];
+			int day = d[1];
+			int year = d[2];
+			
+			int vehicle = Integer.parseInt(itr.nextToken().trim());
+			int trip = Integer.parseInt(itr.nextToken().trim());
 			
 			LocalDate date = LocalDate.of(year, month, day);
 			DayOfWeek dayOfWeek = date.getDayOfWeek();
 			int dayOfWeekNumber = dayOfWeek.getValue();
 			
-			String week = "";
-			switch (dayOfWeekNumber) {
-				case 1:
-					week = "MON";
-					break;
-				case 2:
-					week = "TUE";
-					break;
-				case 3:
-					week = "WED";
-					break;
-				case 4:
-					week = "THR";
-					break;
-				case 5:
-					week = "FRI";
-					break;
-				case 6:
-					week = "SAT";
-					break;
-				default:
-					week = "SUN";
-					break;
-			}
-			one_key.set(token[0] + "," + week);
-			one_value.set(token[2] + "," + token[3]);
-			context.write(one_key, one_value);
+			word.set(baseNum + "," + dayOfWeekNumber);
+			value.set(trip + "," + vehicle);
+			context.write(word, value);
 		}	
 	}
 	
-	public static class UBERStudent20191011 extends Reducer<Text,Text,Text,Text> {
-//		private Text result = new Text();
-//		
+	public static class UBERStudent20191011Reducer extends Reducer<Text,Text,Text,Text> {
+		private Text result = new Text();
+		
 		public void reduce(Text key, Iterable<Text> values, Context context ) throws IOException, InterruptedException {
+			int vehiclesSum = 0;
+			int tripsSum = 0;
+			
+			StringTokenizer itr = new StringTokenizer(key.toString(), ",");
+			String baseNum = itr.nextToken().trim();
+			int day = Integer.parseInt(itr.nextToken().trim());
+			String dayOfWeek = dayOfWeek(day);
+			key.set(baseNum + "," + dayOfWeek);
+			
 			for (Text val : values) {
-				result.set(val);
-				context.write(key, result);
+				itr = new StringTokenizer(val.toString(), ",");
+				int trip = Integer.parseInt(itr.nextToken().trim());
+				int vehicle = Integer.parseInt(itr.nextToken().trim());
+				
+				vehiclesSum += vehicle;
+				tripsSum += trip;
 			}
+			result.set(tripsSum + "," + vehiclesum);
+			context.write(key, result);
 		}
 	}
 
@@ -81,6 +97,8 @@ public class UBERStudent20191011
 		job.setReducerClass(UBERStudent20191011Reducer.class);
 		job.setOutputKeyClass(Text.class);
 		job.setOutputValueClass(Text.class);
+// 		job.setInputFormatClass(TextInputFormat.class);
+// 		job.setOutputFormatClass(TextOutputFormat.class);
 		FileInputFormat.addInputPath(job, new Path(args[0]));
 		FileOutputFormat.setOutputPath(job, new Path(args[1]));
 		FileSystem.get(job.getConfiguration()).delete( new Path(args[1]), true);
